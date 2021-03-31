f6ae837950336333ebac664455d6618dd8e5dfdc
Mahdi-Soultana <soultana.mahdi24@gmail.com>
2021-03-30 19:51:27 -0800

    finshed

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-const navCenter = document.querySelector( ".nav-center" );
-const toggle = document.querySelector( ".close" );
-const signUp = document.querySelector( ".signUp" );
+const navCenter = document.querySelector(".nav-center");
+const toggle = document.querySelector(".close");
+const signUp = document.querySelector(".signUp");
 
-toggle.addEventListener( "click", () => {
- 
-    navCenter.classList.toggle("active")
-})
-window.addEventListener( "scroll", () => {
-    if ( window.scrollY > 200 ) {
-        navCenter.classList.add( "scroll" );
-        signUp.style.margin="0"
-    } else {
-        signUp.style.margin="2vw 3vw"
-        navCenter.classList.remove("scroll")
- }
-    navCenter.classList.remove("active")
-})
+toggle.addEventListener("click", () => {
+  navCenter.classList.toggle("active");
+});
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 200) {
+    navCenter.classList.add("scroll");
+    signUp.style.margin = "0";
+  } else {
+    signUp.style.margin = "2vw 3vw";
+    navCenter.classList.remove("scroll");
+  }
+  navCenter.classList.remove("active");
+});
